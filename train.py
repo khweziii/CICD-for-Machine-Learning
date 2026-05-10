31e6daf683be81582ccc285b515bf3bd265764d6
Khwezi Kgalema <92418710+khweziii@users.noreply.github.com>
2026-05-10 17:00:19 +0200

    Fix file paths for results and model saving

--- train.py
+++ train.py
@@ -45,7 +45,7 @@ predictions = pipe.predict(X_test)
 accuracy = accuracy_score(y_test, predictions)
 f1 = f1_score(y_test, predictions, average="macro")
 
-with open("Results/metrics.txt", "w") as outfile:
+with open("results/metrics.txt", "w") as outfile:
     outfile.write(f"\nAccuracy = {round(accuracy, 2) * 100}, F1 Score = {round(f1, 2)}.")
 
 
@@ -53,8 +53,8 @@ with open("Results/metrics.txt", "w") as outfile:
 cm = confusion_matrix(y_test, predictions, labels=pipe.classes_)
 disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=pipe.classes_)
 disp.plot()
-plt.savefig("Results/model_results.png", dpi=120)
+plt.savefig("results/model_results.png", dpi=120)
 
-sio.dump(pipe, "Model/drug_pipeline.skops")
+sio.dump(pipe, "model/drug_pipeline.skops")
 
-sio.load("Model/drug_pipeline.skops", trusted=[np.dtype])
+sio.load("model/drug_pipeline.skops", trusted=[np.dtype])
